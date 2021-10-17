ae0cd4c36331d2363f9143b9a59aa2d7801b7f66
rishabh-chauhan24 <92340350+rishabh-chauhan24@users.noreply.github.com>
2021-10-17 13:22:51 +0530

    Add files via upload

---
+++ chack.cpp
@@ -0,0 +1,17 @@
+//
+
+#include <iostream>
+using namespace std;
+ 
+int fun(int i)
+{
+  if (i % 2) return (i++);
+  else return fun(fun(i - 1));
+}
+  
+int main()
+{
+  cout << " " << fun(200) << " ";
+  getchar();
+  return 0;
+}
